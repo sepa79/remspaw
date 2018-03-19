67a23669b62c1793a0a22f4f7c4d04795cb0f1cb
sepa <sepa79@gmail.com>
2018-03-19 21:29:54 +0000

    favicon

--- navbar.php
+++ navbar.php
@@ -1,13 +1,10 @@
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
     <a class="navbar-brand" href="/index.php">Rem-Spaw</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+    <div class="collapse navbar-collapse">
     <ul class="navbar-nav mr-auto">
         <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stany</a>
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Stany</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
             <a class="dropdown-header">Urządzenia na serwisie</a>
             <a class="dropdown-item" href="/stan/spawarki.php">Spawarki</a>
@@ -16,7 +13,7 @@
         </div>
         </li>
         <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Przyjęcia</a>
+        <a class="nav-link dropdown-toggle" href="#"data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Przyjęcia</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
             <a class="dropdown-header">Przyjęcie do naprawy</a>
             <a class="dropdown-item" href="/przyjecia/spawarka.php">Spawarka</a>
@@ -25,7 +22,7 @@
         </div>
         </li>
         <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Warsztat</a>
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Warsztat</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
             <a class="dropdown-header">Lista</a>
             <a class="dropdown-item" href="/warsztat/spawarki.php">Spawarki</a>
@@ -41,7 +38,7 @@
     </ul>
     <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle navbar-right" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Czary i magia</a>
+        <a class="nav-link dropdown-toggle navbar-right" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Czary i magia</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
             <a class="dropdown-item" href="http://192.168.0.60:9000">Cerebro</a>
             <div class="dropdown-divider"></div>
